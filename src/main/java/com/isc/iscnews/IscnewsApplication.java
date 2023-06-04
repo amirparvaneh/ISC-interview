98eee5c546dda7709701aa51ead7ee696f75d2c2
amirparvaneh <amirparvaneh138@gmail.com>
2023-06-04 16:52:40 +0330

    dev-first-commit

---
+++ src/main/java/com/isc/iscnews/IscnewsApplication.java
@@ -0,0 +1,13 @@
+package com.isc.iscnews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IscnewsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IscnewsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/isc/iscnews/IscnewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isc.iscnews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IscnewsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
