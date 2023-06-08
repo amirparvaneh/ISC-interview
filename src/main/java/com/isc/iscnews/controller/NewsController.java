8e7a1f467ce2e54cd4c48f67d8b7cbf660b44ff8
amirparvaneh <amirparvaneh138@gmail.com>
2023-06-08 15:35:42 +0330

    web-client

--- src/main/java/com/isc/iscnews/controller/NewsController.java
+++ src/main/java/com/isc/iscnews/controller/NewsController.java
@@ -32,9 +32,4 @@ public class NewsController {
         return newsServiceImpl.getNews(newsRequestDto);
     }
 
-
-    @GetMapping
-    public List<News> getNewsByOpenFeign(@RequestParam String country, @RequestParam String category) {
-        return newsServiceImpl.getNewsByOpenFeign(country,category);
-    }
 }
--- src/main/java/com/isc/iscnews/service/serviceImpl/NewsServiceImpl.java
+++ src/main/java/com/isc/iscnews/service/serviceImpl/NewsServiceImpl.java
@@ -3,7 +3,6 @@ package com.isc.iscnews.service.serviceImpl;
 import com.isc.iscnews.config.WebProxy;
 import com.isc.iscnews.model.News;
 import com.isc.iscnews.model.dto.NewsRequestDto;
-import com.isc.iscnews.proxy.OpenFeignProxy;
 import com.isc.iscnews.repository.NewsRepo;
 import com.isc.iscnews.service.NewsService;
 import org.springframework.stereotype.Service;
@@ -18,23 +17,16 @@ public class NewsServiceImpl implements NewsService {
 
     private final WebProxy webProxy;
 
-    private final OpenFeignProxy openFeignProxy;
 
-    public NewsServiceImpl(NewsRepo newsRepo,WebProxy webProxy,OpenFeignProxy openFeignProxy) {
+    public NewsServiceImpl(NewsRepo newsRepo,WebProxy webProxy) {
         this.newsRepo = newsRepo;
         this.webProxy = webProxy;
-        this.openFeignProxy = openFeignProxy;
     }
 
     @Override
     public Mono<List<News>> getNews(NewsRequestDto newsRequestDto) {
         Mono<List<News>> news = webProxy.getNewsReactive(newsRequestDto);
         return news;
-    }
-
-    @Override
-    public List<News> getNewsByOpenFeign(String country, String category) {
-        return openFeignProxy.getNewsByFeign(country,category);
     }
 
 
