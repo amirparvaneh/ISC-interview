7994a1e63bc33921a8fcae6ea86bff32d5c13f07
amirparvaneh <amirparvaneh138@gmail.com>
2023-06-09 02:57:59 +0330

    openfeign

--- src/main/java/com/isc/iscnews/service/serviceImpl/NewsServiceImpl.java
+++ src/main/java/com/isc/iscnews/service/serviceImpl/NewsServiceImpl.java
@@ -3,6 +3,7 @@ package com.isc.iscnews.service.serviceImpl;
 import com.isc.iscnews.config.WebProxy;
 import com.isc.iscnews.model.News;
 import com.isc.iscnews.model.dto.NewsRequestDto;
+import com.isc.iscnews.proxy.OpenFeignProxy;
 import com.isc.iscnews.repository.NewsRepo;
 import com.isc.iscnews.service.NewsService;
 import org.springframework.beans.factory.annotation.Value;
@@ -18,13 +19,16 @@ public class NewsServiceImpl implements NewsService {
 
     private final WebProxy webProxy;
 
+    private final OpenFeignProxy openFeignProxy;
+
     @Value("${newsApi.apiKey}")
     private String apiKey;
 
 
-    public NewsServiceImpl(NewsRepo newsRepo,WebProxy webProxy) {
+    public NewsServiceImpl(NewsRepo newsRepo, WebProxy webProxy, OpenFeignProxy openFeignProxy) {
         this.newsRepo = newsRepo;
         this.webProxy = webProxy;
+        this.openFeignProxy = openFeignProxy;
     }
 
     @Override
@@ -32,6 +36,15 @@ public class NewsServiceImpl implements NewsService {
         newsRequestDto.setApiKey(apiKey);
         Mono<List<News>> news = webProxy.getNewsReactive(newsRequestDto);
         return news;
+    }
+
+    @Override
+    public List<News> getNewsWithFeign(NewsRequestDto newsRequestDto) {
+        newsRequestDto.setApiKey(apiKey);
+        return openFeignProxy.getNews(
+                newsRequestDto.getCountry(),
+                newsRequestDto.getCategory(),
+                newsRequestDto.getApiKey());
     }
 
 
